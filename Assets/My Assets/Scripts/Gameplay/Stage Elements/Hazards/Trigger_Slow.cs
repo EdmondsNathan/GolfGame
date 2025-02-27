04427ddd29f523f5c72dc29c5d8fa427da87f3d5
Skylar <edmondsnathan1998@gmail.com>
2025-02-27 09:37:39 -0600

    Implements slow fields

---
+++ Assets/My Assets/Scripts/Gameplay/Stage Elements/Hazards/Trigger_Slow.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_Slow : MonoBehaviour
+{
+	#region Fields
+	[SerializeField] private float _slowRate = .1f;
+
+	private List<Rigidbody2D> _rigidbodies = new();
+	#endregion
+
+	#region Unity methods
+	protected void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.TryGetComponent<Rigidbody2D>(out var newRigidbody))
+		{
+			_rigidbodies.Add(newRigidbody);
+		}
+	}
+
+	protected void OnTriggerExit2D(Collider2D collider)
+	{
+		_rigidbodies.Remove(collider.attachedRigidbody);
+	}
+
+	protected void FixedUpdate()
+	{
+		foreach (Rigidbody2D rb in _rigidbodies)
+		{
+			//rb.AddForce(_slowRate * transform.TransformDirection(_forceDirection), ForceMode2D.Force);
+			rb.linearVelocity *= 1 - (_slowRate * Time.fixedDeltaTime);
+		}
+	}
+	#endregion
+}
