f81458dba40fa285b5da1fc242ddc72805ef7d6c
Skylar <edmondsnathan1998@gmail.com>
2025-02-27 09:56:13 -0600

    fixes missing invoke on event call

--- Assets/My Assets/Scripts/Menus/ButtonBehaviors/SelectGolfBall.cs
+++ Assets/My Assets/Scripts/Menus/ButtonBehaviors/SelectGolfBall.cs
@@ -5,7 +5,7 @@ public class SelectGolfBall : SingletonMonoBehaviour<SelectGolfBall>
 	#region Public methods
 	public void Select(GameObject abilityPrefab)
 	{
-		Messages_SelectGolfBall.OnGolfBallSelected(abilityPrefab);
+		Messages_SelectGolfBall.OnGolfBallSelected?.Invoke(abilityPrefab);
 	}
 	#endregion
 }
