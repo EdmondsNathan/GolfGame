7c1c74647335b03a3fb44031d85fb5fc3d7f7ba6
Skylar <edmondsnathan1998@gmail.com>
2025-02-27 09:06:39 -0600

    Fixed a missing null check for message

--- Assets/My Assets/Scripts/Menus/ButtonBehaviors/SelectAbility.cs
+++ Assets/My Assets/Scripts/Menus/ButtonBehaviors/SelectAbility.cs
@@ -5,7 +5,7 @@ public class SelectAbility : SingletonMonoBehaviour<SelectAbility>
 	#region Public methods
 	public void Select(GameObject abilityPrefab)
 	{
-		Messages_SelectAbility.OnAbilitySelected(abilityPrefab);
+		Messages_SelectAbility.OnAbilitySelected?.Invoke(abilityPrefab);
 	}
 	#endregion
 }
